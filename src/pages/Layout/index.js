91a9a52de75e4c27b1429217433919b582a22f60
Ian <shameless@galage.cn>
2022-12-11 15:28:55 +0800

    antd组件库使用

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react'
+import { Button, Space } from 'antd'
 export default class Layout extends Component {
   render() {
-    return <div>首页布局组件</div>
+    return (
+      <div>
+        首页布局组件
+        <Space wrap>
+          <Button type="primary">Primary Button</Button>
+          <Button>Default Button</Button>
+          <Button type="dashed">Dashed Button</Button>
+          <Button type="text">Text Button</Button>
+          <Button type="link">Link Button</Button>
+        </Space>
+      </div>
+    )
   }
 }
